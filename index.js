54abfa001f82df695886109674bacbf4bf6312fe
Janice Teguh <janicejarusdy@gmail.com>
2022-11-18 17:57:08 +1100

    unsave post working when button is clicked in search and results, but not from favourite comics

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-$(window).on('load', function(){
+$(window).on('load', function () {
     $(".loader").fadeOut(2000)
     $(".content").fadeIn(2000)
 })
 
-const searchTool = document.querySelector("div #searchTool-link")
-const faveComics = document.querySelector("div #faveComics-link")
+
+const searchToolLink = document.querySelector("#searchTool-link")
+const faveComicsLink = document.querySelector("#faveComics-link")
+
+//add event listeners for the above two links, making display none for each other
+const searchTool = document.querySelector("div .searchTool")
+const faveComics = document.querySelector("div #faveComics")
 const showBoth = document.querySelector("div #showBoth-link")
 
 searchToolLink.addEventListener("click", () => {
-    faveComics.style.display="none"
-    searchTool.style.display="block"
+    faveComics.style.display = "none"
+    searchTool.style.display = "block"
 })
 
 faveComicsLink.addEventListener("click", () => {
-    searchTool.style.display="none"
-    faveComics.style.display="block";
+    searchTool.style.display = "none"
+    faveComics.style.display = "block";
 })
 
 showBoth.addEventListener("click", () => {
-    searchTool.style.display="block"
-    faveComics.style.display="block";
+    searchTool.style.display = "block"
+    faveComics.style.display = "block";
 })
 
 async function getNumberOfComics() {
@@ -33,16 +38,16 @@ async function getNumberOfComics() {
 
 const promises = []
 
-async function getAllComics(comicArray){
+async function getAllComics(comicArray) {
     const numberOfComics = await getNumberOfComics()
     for (let i = 1; i <= numberOfComics; i++) {
         fetch(`https://xkcd.com/${i}/info.0.json`)
-        .then(res => {
-            promises.push(res)
-            return res.json()
-        })
-        .then(data => comicArray.push(data))
-        .catch(error => console.log(`error at id ${i}`))
+            .then(res => {
+                promises.push(res)
+                return res.json()
+            })
+            .then(data => comicArray.push(data))
+            .catch(error => console.log(`error at id ${i}`))
     }
 }
 
@@ -51,7 +56,7 @@ getAllComics(allComics)
 
 const ul = document.querySelector('#searchResults ul')
 const inputForm = document.querySelector('form');
-  
+
 inputForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const input = document.querySelector('input#searchByKeyword');
@@ -68,7 +73,7 @@ async function filterComics(keyword) {
     searchResults.innerText = `Search Results for "${keyword}"`
 
     await Promise.allSettled(promises)
-    .then(findMatchingComics(allComics, keyword))
+        .then(findMatchingComics(allComics, keyword))
 }
 
 function findMatchingComics(comicArray, keyword) {
@@ -97,25 +102,20 @@ function findMatchingComics(comicArray, keyword) {
     ul.innerHTML += '<h3 class="search-posts"> - End of results - </h4>'
 
     console.log(allComics.length)
-            
+
 }
 
 function toggleFaveComic(event) {
-    console.log(`the id is: ${event.target.dataset.id}`)
     if (event.target.dataset.id) {
-        console.log(`I'm in toggleFaveComic trying to delete ${event.target}`)
         deleteFaveComic(event.target)
     }
     else {
         addFaveComic(event.target)
-        console.log(`I'm in toggleFaveComic trying to add ${event.target}`)
     }
-
-    console.log(`the id is now: ${event.target.dataset.id}`)
 }
 
 
-async function addFaveComic(element) {
+function addFaveComic(element) {
     const postObj = {
         method: "POST",
         headers: {
@@ -127,22 +127,18 @@ async function addFaveComic(element) {
             img: element.dataset.img,
             alt: element.dataset.alt,
             num: element.dataset.num
-            
+
         })
     }
 
-    await fetch('http://localhost:3000/faveComics', postObj)
+    fetch('http://localhost:3000/faveComics', postObj)
         .then(res => res.json())
         .then(data => {
-            element.dataset.id = data.id
 
-            console.log(`element = ${element}`)
-            console.log(`data = ${data}`)
-        
             const ulBookmarks = document.querySelector("#faveComics ul")
 
             ulBookmarks.innerHTML += `
-            <li class="bookmarks" data-num="${element.dataset["num"]}}>
+            <li class="bookmarks" data-num="${element.dataset["num"]}"}>
                 <div class = "container">
                     <div class= "comic" >
                     <h2>${element.dataset["title"]}</h2>
@@ -155,58 +151,34 @@ async function addFaveComic(element) {
             `
 
             const saveButton = document.querySelector(`div[data-num="${element.dataset["num"]}"]`)
-            
+
             saveButton.innerHTML = `
-            <button class="saved-btn" data-id="${data.id}" onClick="toggleFaveComic(event)">Saved</button>
+            <button class="saved-btn" data-id="${data.id}" data-num="${element.dataset["num"]}" onClick="toggleFaveComic(event)">Saved</button>
             `
             saveButton.dataset.id = data.id
             saveButton.classList.remove("unsaved-btn")
             saveButton.classList.add("saved-btn")
             saveButton.parentNode.dataset.id = data.id
+
+            numLiked.push(`${element.dataset["num"]}`)
         })
 
-        const button = document.querySelector('button')
-        button.addEventListener("click", ((event)=>console.log(event.target)))
 }
 
 // the below function should have a functionality to delete li item
 // on FaveComics page based on their "id" number aka db.json id number
 function deleteFaveComic(element) {
-    const resetComicObj = {
-        title: element.dataset.title,
-        img: element.dataset.img,
-        alt: element.dataset.alt,
-        num: element.dataset.num,
-        id: 0
-    }
-    const initObj = {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            "Accepts": "application/json"
-        },
-        body: JSON.stringify(
-            resetComicObj
-        )
-            
-        
-    }
+    console.log(element)
+    const likedComic = document.querySelector(`li[data-num="${element.dataset["num"]}"`)
+    console.log(likedComic)
+    const greenButton = document.querySelector(`div[data-num="${element.dataset["num"]}"]`)
+    console.log(greenButton)
 
-    fetch(`'http://localhost:3000/faveComics/${element.dataset.id}`, initObj)
-        .then(res => res.json())
-        .then(data => {
-            delete data
-
-            const liSaved = document.querySelector(`li[data-num="${element.dataset["num"]}"}`)
-            liSaved.innerHTML = ''
-
-            const greenButton = document.querySelector(`div[data-num="${element.dataset["num"]}"]`)
-            
-            greenButton.classList.remove("saved-btn")
-            greenButton.classList.add("unsaved-btn")
-            greenButton.innerHTML = `
+    likedComic.innerHTML = ''
+    greenButton.classList.remove("saved-btn")
+    greenButton.classList.add("unsaved-btn")
+    greenButton.innerHTML = `
                 <button onClick="toggleFaveComic(event)">Save to Bookmarks</button>
             `
-        })
 
 }
